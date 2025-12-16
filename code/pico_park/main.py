a1dc2c1bb0e9fab16e756f7145a1dd623241ada2
kyriosaa <kyriosaaph@gmail.com>
2025-12-17 00:46:45 +0800

    Revert "- using single main.py cause some bugs and confliction."
    This reverts commit a670a6b1bec976cd51a355f815aa7d7842677979.

--- code/pico_park/main.py
+++
@@ -1,9 +0,0 @@
-# main.py (SAFE BOOT FILE)
-# This file should stay simple to avoid blocking Thonny
-
-import time
-
-print("Pico A booted. main.py finished.")
-
-# Do NOTHING else here
-# You will manually run server.py from Thonny

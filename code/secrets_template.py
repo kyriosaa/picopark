10152f219101f6cc7f62343ea46a8cbe390c7900
ACrousi <0625sean@gmail.com>
2025-12-19 15:29:37 +0800

    remove secret_template.py

--- code/secrets_template.py
+++
@@ -1,9 +0,0 @@
-# Secrets Template
-# Rename this file to secrets.py and fill in your details
-
-SSID = "YOUR_WIFI_SSID"
-PASSWORD = "YOUR_WIFI_PASSWORD"
-
-# IP address of the Pico Gate (Server)
-# You can find this on the OLED screen of the Gate
-GATE_IP = "192.168.1.100" 
